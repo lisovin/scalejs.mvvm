ef7094f5366c1f86668fb906df657e65a07d4063
Peter Lisovin <peter@lisovin.com>
2013-01-16 15:55:04 -0500

    Updated knockoutjs reference.

--- scalejs.mvvm/bootstrapper.js
+++ scalejs.mvvm/bootstrapper.js
@@ -1,7 +1,7 @@
 /*global require*/
 require({
     "paths":  {
-        "knockout":  "Scripts/knockout-2.2.0.debug",
+        "knockout":  "Scripts/knockout-2.2.1",
         "knockout-classBindingProvider":  "Scripts/knockout-classBindingProvider",
         "knockout.mapping":  "Scripts/knockout.mapping-latest.debug",
         "scalejs":  "Scripts/scalejs-0.1.11"
--- scalejs.mvvm/tools/install.ps1
+++ scalejs.mvvm/tools/install.ps1
@@ -4,7 +4,7 @@ $project |
 	Add-Paths "{
 		'scalejs.mvvm'					: 'Scripts/scalejs.mvvm-$($package.Version)',
 		'text'							: 'Scripts/text',
-		'knockout'						: 'Scripts/knockout-2.2.0',
+		'knockout'						: 'Scripts/knockout-2.2.1',
 		'knockout.mapping'				: 'Scripts/knockout.mapping-latest',
 		'knockout-classBindingProvider' : 'Scripts/knockout-classBindingProvider.min'
 	}" |
